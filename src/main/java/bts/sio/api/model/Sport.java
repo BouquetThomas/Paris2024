0b4e195058976800a621df340c3676258b861b31
Lucas <lucasjungels@gmail.com>
2023-10-09 13:58:54 +0200

    Mise à jour de Sport pour être conforme au nouveau code.

--- src/main/java/bts/sio/api/model/Sport.java
+++ src/main/java/bts/sio/api/model/Sport.java
@@ -4,13 +4,11 @@ package bts.sio.api.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import javax.lang.model.element.NestingKind;
-import java.util.Set;
-
 @Data
 @Entity
-@Table(name ="sport")
-    public class Sport {
+@Table(name = "sport")
+public class Sport {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,13 +19,10 @@ import java.util.Set;
     @Column(name = "descriptif")
     private String descriptif;
 
-    @Column(name = "nom_image")
-    private String nom_image;
+    @Column(name = "nomImage")
+    private String nomImage;
 
     @ManyToOne
     @JoinColumn(name = "olympiade_id")
     private Olympiade olympiade;
-
-    @OneToMany (mappedBy = "sport")
-    private Set<Epreuve> LesEpreuves;
 }
